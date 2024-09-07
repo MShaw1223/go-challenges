e6e7588260085ce074b551469cd86ebc39e0c84b
Miller <millershaw61@gmail.com>
2024-09-07 02:18:13 +0100

    Init
    Initial commit of first data structures work

---
+++ data-structures/main.go
@@ -0,0 +1,74 @@
+package main
+
+import "fmt"
+
+func main(){
+	array()
+	tuple()
+	hash_map()
+}
+
+func array(){
+	var input string
+	var a [5]string
+
+	for i := 0; i < len(a); i++ {
+		fmt.Printf("Enter word number %d here: ",i + 1)
+		_,err := fmt.Scanln(&input);
+		if err != nil {
+			fmt.Println("Error reading input:",err ,"at index:",i)
+			return
+		}
+		a[i] = input
+	}
+	fmt.Println("Array of your words:",a)
+}
+
+func tuple(){
+	type tuple struct {
+		First int
+		Second string
+	}
+
+	var input string
+	var int_input int
+
+	fmt.Print("Enter a string: ")
+	_,f_err := fmt.Scanln(&input)
+	if f_err != nil {
+		fmt.Printf("Error in string scanln: %T",f_err)
+	}
+
+	fmt.Print("Enter an integer: ")
+	_,s_err:= fmt.Scanln(&int_input)
+	if s_err != nil {
+		fmt.Printf("Error in int scanln: %T",s_err)
+	}
+
+	t:= tuple{First: int_input, Second: input}
+
+	fmt.Println("Tuple:",t)
+}
+
+func hash_map(){
+	age_map := make(map [string] int)
+
+	var age int
+	var name string
+
+	fmt.Print("Enter an age: ")
+	_,a_err := fmt.Scanln(&age)
+	if a_err !=nil {
+		fmt.Println("Error reading age input:", a_err)
+	}
+
+	fmt.Print("Enter a name: ")
+	_, n_err:=fmt.Scanln(&name)
+	if n_err != nil {
+		fmt.Println("Error reading name input: ",n_err)
+	}
+
+	age_map[name] = age
+	fmt.Print(name,"'s ")
+	fmt.Println("age:",age_map[name])
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/MShaw1223/go-challenges.git
+
+go 1.23.0
